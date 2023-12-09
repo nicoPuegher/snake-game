87e6f314101a4cbd51723255a344a5109349b2c0
Nicolas <nicopuegher@gmail.com>
2023-12-08 21:01:29 -0300

    Declare basic variables to draw the snake

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+-- Window/screen
+local window_width, window_height
+
+-- Player
+local snake, block_width, block_height
+
+-- Functions
+local rectangle
+
+-- Constants
+local SIZE = 20
+
 function love.load() end
 
 function love.update() end
