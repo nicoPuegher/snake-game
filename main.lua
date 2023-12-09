123d7794cc48b859c3c8a5636c69b41ea021fbc4
Nicolas <nicopuegher@gmail.com>
2023-12-08 21:26:02 -0300

    Initialize the snake's body in a table of blocks

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ local rectangle
 
 -- Constants
 local SIZE = 20
+local INITIAL_SNAKE_SIZE = 3
 
 function love.load()
 	-- Get window size
@@ -18,6 +19,15 @@ function love.load()
 	-- Calculate the size of a single block of the snake's body
 	block_width = window_width / SIZE
 	block_height = window_height / SIZE
+
+	-- Initialize the snake's body
+	snake = {}
+	for i = 1, INITIAL_SNAKE_SIZE do
+		local snake_body = {}
+		snake_body.x = block_width * i
+		snake_body.y = block_height
+		table.insert(snake, snake_body)
+	end
 end
 
 function love.update() end
