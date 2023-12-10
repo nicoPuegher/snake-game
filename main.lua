db5f96458f6b579cb63d656dfec1f8cfb83f16ba
Nicolas <nicopuegher@gmail.com>
2023-12-09 22:50:22 -0300

    Control the speed of the game

--- main.lua
+++ main.lua
@@ -4,13 +4,17 @@ local window_width, window_height
 -- Player
 local snake, block_width, block_height, direction
 
+-- Game
+local timer
+
 -- Functions
 local rectangle
 
 -- Constants
 local SIZE = 20
 local INITIAL_SNAKE_SIZE = 3
-local SNAKE_SPEED = 0.15
+local SNAKE_SPEED = 9
+local DURATION = 1 / SNAKE_SPEED
 
 function love.load()
 	-- Get window size
@@ -32,19 +36,32 @@ function love.load()
 
 	-- Initialize the snake's direction
 	direction = "right"
+
+	-- Initialize accumulative timer
+	timer = 0
 end
 
-function love.update()
-	local snake_head = { x = snake[#snake].x, y = snake[#snake].y }
+function love.update(dt)
+	-- Accumulate time to control the speed of the snake
+	timer = timer + dt
 
-	love.timer.sleep(SNAKE_SPEED)
+	-- When the timer exceedes the duration, there is room for an update
+	while timer >= DURATION do
+		-- Create a new snake head
+		local snake_head = { x = snake[#snake].x, y = snake[#snake].y }
 
-	if direction == "right" then
-		snake_head.x = snake_head.x + block_width
+		-- Move the new snake head based on the current direction
+		if direction == "right" then
+			snake_head.x = snake_head.x + block_width
+		end
+
+		-- Insert the new snake head and remove the tail
+		table.insert(snake, snake_head)
+		table.remove(snake, 1)
+
+		-- Substract one frame from the accumulated time
+		timer = timer - DURATION
 	end
-
-	table.insert(snake, snake_head)
-	table.remove(snake, 1)
 end
 
 function love.draw()
